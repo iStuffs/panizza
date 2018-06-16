5cb2312829ed57a1d070cf437ab8a48e727a545b
iStuffs Samuel DIDIER-LAURENT <istuffs@gmail.com>
2018-06-16 11:05:12 +0200

    add gulp modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,39 @@
-var gulp   = require('gulp');
-var panini = require('panini');
+var gulp            = require('gulp');
+var panini          = require('panini');
+var gulpif          = require('gulp-if');
+var sass            = require('gulp-sass');
+var cssMinification = require('gulp-clean-css');
+var args            = require('yargs').argv;
+var autoprefixer    = require('gulp-autoprefixer');
+var notify          = require("gulp-notify");
+var sourcemaps      = require('gulp-sourcemaps');
 
 /* configuration */
 var PATH = {
   src: 'src/',
+  dist: './dist/',
 };
 
 PATH.panini = {
   src:      PATH.src + 'pages/**/*.html',
   root:     PATH.src + 'pages/',
-  layouts:  PATH.src + 'root/layouts/',
-  partials: PATH.src + 'root/partials/',
-  helpers:  PATH.src + 'root/helpers/',
-  data:     PATH.src + 'root/data/',
+  layouts:  PATH.src + 'layouts/',
+  partials: PATH.src + 'partials/',
+  helpers:  PATH.src + 'helpers/',
+  data:     PATH.src + 'data/',
 }
 
 PATH.css = {
-  src:      PATH.src + 'assets/sass/**/*.html',
-  data:     PATH.src + 'root/data/',
+  src:      PATH.src  + 'assets/sass/**/*.{sass,scss}',
+  dest:     PATH.dist + 'css/'
 }
 
+PATH.js = {
+  src:      PATH.src + 'assets/script/**/*.js',
+  dest:     PATH.src + 'js/'
+}
+
+/* panini templating */
 gulp.task('panini', function() {
   return gulp.src(PATH.panini.src)
     .pipe(panini({
@@ -38,11 +52,26 @@ gulp.task('panini:refresh', function(done) {
   done();
 });
 
+/* CSS */
+gulp.task('cssTask', function () {
+  return gulp.src(PATH.css.src)
+  .pipe(gulpif(!args.production, sourcemaps.init()))
+  .pipe(sass().on('error', sass.logError))
+  .pipe(autoprefixer({
+      browsers: ['last 3 versions'],
+      cascade: false
+  }))
+  .pipe(gulpif(!!args.production, cssMinification({compatibility: 'ie9'})))
+  .pipe(gulpif(!args.production, sourcemaps.write('.')))
+  // .pipe(gulp.dest(PATH.css.dist));
+  .pipe(gulp.dest('./dist/css/'));
+});
 
-gulp.task('watch', ['panini'], function() {
-  gulp.watch( 'src/{layouts,partials,helpers,data}/**/*',
-  ['panini:refresh', 'panini']
-);
+/* watching */
+
+gulp.task('watch', ['cssTask', 'panini'], function() {
+  gulp.watch('./src/assets/sass/**/*.{sass,scss}', ['cssTask']);
+  gulp.watch( 'src/{layouts,partials,helpers,data}/**/*', ['panini:refresh', 'panini']);
 });
 
 gulp.task('default', ['watch']);
