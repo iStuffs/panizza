5fe45e3f7a545c6a6795936d48e014063ae3c5b3
Samuel DIDIER-LAURENT <istuffs@gmail.com>
2018-12-12 17:30:56 +0100

    refactor(gulpfiles): outsource config to external file

--- gulpfile.js
+++ gulpfile.js
@@ -8,30 +8,10 @@ const autoprefixer    = require('gulp-autoprefixer');
 const notify          = require("gulp-notify");
 const sourcemaps      = require('gulp-sourcemaps');
 
+
 /* configuration */
-const PATH = {
-  src: 'src/',
-  dist: './dist/',
-};
-
-PATH.panini = {
-  src:      PATH.src + 'pages/**/*.html',
-  root:     PATH.src + 'pages/',
-  layouts:  PATH.src + 'layouts/',
-  partials: PATH.src + 'partials/',
-  helpers:  PATH.src + 'helpers/',
-  data:     PATH.src + 'data/',
-}
-
-PATH.css = {
-  src:      PATH.src  + 'assets/sass/**/*.{sass,scss}',
-  dest:     PATH.dist + 'css/'
-}
-
-PATH.js = {
-  src:      PATH.src + 'assets/script/**/*.js',
-  dest:     PATH.src + 'js/'
-}
+const { PATH } = require('./config.json');
+const production = !!args.production;
 
 /* panini templating */
 gulp.task('panini', () => {
@@ -43,9 +23,8 @@ gulp.task('panini', () => {
       helpers:  PATH.panini.helpers,  // .js
       data:     PATH.panini.data      //JSON (.json) or YAML (.yml)
     }))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest(PATH.dest));
 });
-
 
 gulp.task('panini:refresh', (done) =>{
   panini.refresh();
@@ -55,20 +34,18 @@ gulp.task('panini:refresh', (done) =>{
 /* CSS */
 gulp.task('cssTask', () => {
   return gulp.src(PATH.css.src)
-  .pipe(gulpif(!args.production, sourcemaps.init()))
+  .pipe(gulpif(!production, sourcemaps.init()))
   .pipe(sass().on('error', sass.logError))
   .pipe(autoprefixer({
       browsers: ['last 3 versions'],
       cascade: false
   }))
-  .pipe(gulpif(!!args.production, cssMinification({compatibility: 'ie9'})))
-  .pipe(gulpif(!args.production, sourcemaps.write('.')))
-  // .pipe(gulp.dest(PATH.css.dist));
-  .pipe(gulp.dest('./dist/css/'));
+  .pipe(gulpif(production, cssMinification({compatibility: 'ie9'})))
+  .pipe(gulpif(!production, sourcemaps.write('.')))
+  .pipe(gulp.dest(PATH.css.dest));
 });
 
 /* watching */
-
 gulp.task('watch', gulp.series('cssTask', 'panini', () => {
   gulp.watch('./src/assets/sass/**/*.{sass,scss}', gulp.series('cssTask'));
   gulp.watch('src/{layouts,partials,helpers,data}/**/*', gulp.series('panini:refresh', 'panini'));
