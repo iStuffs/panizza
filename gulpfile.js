b07ad31f4416be56825453fffc1ac87488234e43
iStuffs <istuffs@gmail.com>
2019-07-24 11:55:24 +0200

    feature: use new serve task

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ const { series, parallel, watch } = require('gulp')
 const {
   archive,
   assets,
-  browserReload,
   clean,
   config,
   css,
@@ -33,22 +32,22 @@ const production = modeArg
 const build = series(clean, parallel(assets, css, js, images, htmlPanini));
 
 /* Watching */
-const watcher = series(build, serve, () => {
+const watcher = series(build, serve.init, () => {
     // assets
     watch(PATH.src + ASSETS.src, series(assets))
-      .on("all", series(browserReload));
+      .on("all", series(serve.reload));
     // css
     watch(PATH.src + CSS.src, series(css))
-      .on("all", series(browserReload));
+      .on("all", series(serve.reload));
     // html
     watch(PATH.src + HTML.src)
-      .on("all", series(paniniRefresh, htmlPanini, browserReload));
+      .on("all", series(paniniRefresh, htmlPanini, serve.reload));
     // images
     watch(PATH.src + IMAGES.src, series(images))
-      .on("all", series(browserReload));
+      .on("all", series(serve.reload));
     // javascript
     watch(PATH.src + JS.src, series(js))
-      .on("all", series(browserReload));
+      .on("all", series(serve.reload));
   });
 
 module.exports = {
